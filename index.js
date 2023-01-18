95f2951ed9bd83000790e63836604b94cd27a164
pukar <pukarlamichhane567@gmail.com>
2023-01-18 17:01:43 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+
+ReactDOM.render(<><h1>Pukar lamichhane  Netflick pack</h1>,
+<h2>List of best anime series</h2>
+<ol>
+    <li>Naruto</li>
+    <li>One piece</li>
+    <li>dragon ball z</li>
+    <li>Bleach</li>
+    </ol></>,document.getElementById('root'))
